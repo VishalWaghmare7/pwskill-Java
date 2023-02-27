dac190ffe19f8db6ff83c206bed8bdf664e5d663
Vishal Waghmare <89912317+VishalWaghmare7@users.noreply.github.com>
2023-02-27 19:32:32 +0530

    Create area_of_rectangle.java

---
+++ area_of_rectangle.java
@@ -0,0 +1,14 @@
+//Q3 - Write a Java program to take the length and breadth of a rectangle and print its area. (Easy)
+//        Sample Input : 7, 4
+//        Sample Output : 28
+
+public class area_of_rectangle {
+    public static void main(String[] args) {
+        int length = 6;
+        int breadth = 5;
+        int area;
+        System.out.println("The length is :" + length);
+        System.out.println("The breadth is :"+ breadth);
+        System.out.println("The area is :"+ length*breadth);
+    }
+}
